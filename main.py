3ff49ea63f4deed1194d0e8d221ef15344057b87
rishitoshsingh <rishitoshs@gmail.com>
2021-10-25 16:05:10 +0530

    merged

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ triggered = False
 old_frame = None
 frame_i = 0
 
-end_timestamps = ['']*len(slide_files)
+end_timestamps = [np.nan]*len(slide_files)
 start_time = datetime.fromtimestamp(0)
 
 frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
@@ -65,7 +65,7 @@ while True:
         cos_d, sim_index = utils.get_cosine_score(current_frame_feature, slides_feature[current_slide: current_slide + WINDOW_SEARCH_SIZE])
         cos_d *= 100
         
-        org_frame = cv2.putText(org_frame, str(int(cos_d)), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2, cv2.LINE_AA)
+        # org_frame = cv2.putText(org_frame, str(int(cos_d)), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2, cv2.LINE_AA)
         # cv2.imshow('Video', org_frame)
         # cv2.imshow('Slide', org_slides[current_slide])
     
@@ -75,7 +75,7 @@ while True:
             seconds = timedelta(milliseconds=int(timestamp)).total_seconds()
             timestamp = utils.get_time(seconds)
             end_timestamps[current_slide-1] = timestamp
-            print('Topic End Triggered - ',end_timestamps[current_slide-1])
+            print('Slide {} - {}'.format(current_slide-1, end_timestamps[current_slide-1]))
         
     # if cv2.waitKey(1) == ord('q'):
     #     cap.release()
@@ -83,9 +83,82 @@ while True:
 
 end_timestamps[-1] = end_time
 data = {}
-data['slide_no'] = list(range(0, len(slide_files)))
-data['slide'] = [' ']*len(slide_files)
-data['end_time'] = end_timestamps
+data['Slide'] = list(range(0, len(slide_files)))
+data['Image'] = [' ']*len(slide_files)
+data['End Time'] = end_timestamps
 data_df = pd.DataFrame(data)
+
+start_timestamps = ['0:0:0'] + end_timestamps[:-1] 
+data_df['Start Time'] = start_timestamps
+start_time = pd.to_datetime(data_df['Start Time'], format='%H:%M:%S')
+end_time = pd.to_datetime(data_df['End Time'], format='%H:%M:%S')
+
+def get_duration(start, end):
+    duration = [np.nan]*len(end)
+    for i, end_time in enumerate(end):
+        if end_time is pd.NaT:
+            continue
+        start_i = i
+        while start[start_i] is pd.NaT and start_i>=0:
+            start_i -= 1
+        if i == 46:
+            print(start_i)
+        if start[start_i] is not pd.NaT:
+            duration[i] = (end_time - start[start_i]).total_seconds()
+    return duration
+data_df['Duration (s)'] = get_duration(start_time, end_time)
+
+start_time = '11:17:39'
+start_time = datetime.strptime(start_time, '%H:%M:%S')
+from collections import OrderedDict
+import re
+transcript = OrderedDict()
+
+with open('transcripts/apple.txt', 'r') as file:
+    for line in file:
+        time_match = re.search('[0-9]{2}:[0-9]{2}:[0-9]{2}', line)
+        current_time = datetime.strptime(line[time_match.start():time_match.end()], '%H:%M:%S')
+        delta = current_time - start_time
+        delta = utils.get_time(delta.total_seconds())
+        transcript[delta] = line[time_match.end()+1:].strip()
+
+data_df['Captions'] = ['']*len(data_df['Slide'])
+
+def compare_time(t1, t2):
+    if t1 is np.nan or t2 is np.nan:
+        return False
+    t1 = datetime.strptime(t1,"%H:%M:%S")
+    t2 = datetime.strptime(t2,"%H:%M:%S")
+    return t1 >= t2
+
+current_slide = 0
+end_triggered = False
+for cc_time in transcript.keys():
+    if compare_time(data_df['End Time'][current_slide], cc_time):
+        data_df['Captions'][current_slide] = data_df['Captions'][current_slide] + transcript[cc_time] + ' '
+    elif end_triggered:
+        data_df['Captions'][current_slide] = data_df['Captions'][current_slide] + transcript[cc_time] + ' '
+    else:
+        while True:
+            current_slide += 1
+            if current_slide == len(data_df['End Time'])-1:
+                end_triggered = True
+                break
+            elif data_df['End Time'][current_slide] is not np.nan and compare_time(data_df['End Time'][current_slide], cc_time):
+                break
+        data_df['Captions'][current_slide] = data_df['Captions'][current_slide] + ' ' + transcript[cc_time]
+
+data_df['Total Words'] = data_df['Captions'].apply(lambda x: len(x))
+
+wpm = [np.nan] * len(data_df['Captions'])
+for i, (duration, cc) in enumerate(zip(data_df['Duration (s)'], data_df['Captions'])):
+    if cc is np.nan:
+        continue
+    text = cc.split()
+    wpm[i] = len(text) * 60 / duration
+data_df['Words per Minute (wpm)'] = wpm
+
+data_df = data_df[['Slide','Image','Start Time','End Time','Duration (s)', 'Total Words', 'Words per Minute (wpm)', 'Captions']]
+
 excel_name = 'timestamps-apple.xlsx'
 utils.to_excel(excel_name, data_df, slide_files)
